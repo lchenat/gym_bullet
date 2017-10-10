becf3aacafd3fd3bb3a502c5da4c8b54a054591a
liychen <liychen@student.ethz.ch>
2017-10-09 21:21:07 -0700

    add swimmer

--- test_env.py
+++ test_env.py
@@ -1,10 +1,11 @@
 import numpy as np 
 import gym
-from pybullet_my_envs.gym_locomotion_envs import SwimmerBulletEnv
+from pybullet_my_envs.gym_locomotion_envs import SwimmerBulletEnv, Walker2DBulletEnv, HumanoidBulletEnv
+import time
 
-env = SwimmerBulletEnv(render=True)
+# env = Walker2DBulletEnv(render=True)
+env = HumanoidBulletEnv(render=True)
 o = env.reset()
 while True:
-	next_o, r, d, info = env.step(env.action_space.sample())
-	if d:
-		break
+        next_o, r, d, info = env.step(env.action_space.sample())
+        time.sleep(0.05)
