1af18dc693d4fe0bc2aab8ce35c847a4c31b9853
seba-1511 <seba-1511@hotmail.com>
2017-10-10 23:24:23 -0700

    Added environment for high-level policy

---
+++ high_level_env.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python3
+
+import gym
+import gym.spaces
+import numpy as np
+
+from PIL import Image
+from numpy.linalg import norm
+
+
+class StochasticDiscreteOrientation(gym.Env):
+
+    def __init__(self, size_noise=0.1, map_size=50.0, goal_radius=0.05):
+        super(StochasticDiscreteOrientation, self).__init__()
+        self.metadata = {
+            'render.modes':  ['human', 'rgb']
+        }
+        self.size_noise = size_noise
+        self.map_size = map_size
+        self.actions = [
+            np.array([1, 0]),  # right
+            np.array([-1, 0]),  # left
+            np.array([0, 1]),  # up
+            np.array([0, -1]),  # down
+        ]
+        self.goal_radius = map_size * goal_radius
+        self.position = np.zeros((2))
+        self.goal_position = np.random.rand(2) * self.map_size
+        np.clip(self.goal_position, -map_size, map_size, self.goal_position)
+        self.action_space = gym.spaces.Discrete(4)
+        self.observation_space = gym.spaces.Box(low=-map_size,
+                                                high=map_size,
+                                                shape=(4, ))
+
+    @property
+    def _state(self):
+        return np.concatenate([self.position, self.goal_position], axis=0)
+
+    def _step(self, action):
+        move = self.actions[action] + self.size_noise * np.random.randn(1)
+        self.position += move
+        np.clip(self.position, -self.map_size, self.map_size, self.position)
+        dist_to_goal = norm(self.goal_position - self.position)
+        reward = (norm(self.goal_position) - dist_to_goal)
+        reward /= norm(self.goal_position)
+        done = dist_to_goal <= self.goal_radius
+        return self._state, reward, done, None
+
+    def _reset(self):
+        self.position = np.zeros((2))
+        self.goal_position = np.random.rand(2) * self.map_size
+
+    def _render(self, mode='human', close=False):
+        if close:
+            return
+        map_view = np.zeros((int(self.map_size) * 2,
+                             int(self.map_size) * 2,
+                             3), dtype=np.uint8)
+        map_view += 255
+        r = int(self.goal_radius)
+        gp_x, gp_y = self.goal_position.astype('int') + int(self.map_size)
+        map_view[gp_x - r:gp_x + r, gp_y - r:gp_y + r, 1:] = 0
+        p_x, p_y = self.position.astype('int') + int(self.map_size)
+        map_view[p_x - r:p_x + r, p_y - r:p_y + r, :] = 0
+        if mode == 'rgb':
+            return map_view
+        if mode == 'human':
+            img = Image.fromarray(map_view, 'RGB')
+            img.show()
+
+    def _close(self):
+        pass
+
+    def _seed(self, seed=None):
+        np.random.seed(seed)
+
+
+if __name__ == '__main__':
+    asdf = StochasticDiscreteOrientation()
+    asdf.render()
+    for _ in range(1000):
+        asdf.step(0)
+    asdf.render()
